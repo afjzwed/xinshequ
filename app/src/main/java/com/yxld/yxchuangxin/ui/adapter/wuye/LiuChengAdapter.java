2e0853d3341330b24ecf64636ddf90453a45b459
StoneXL <3401794305@qq.com>
2018-05-15 19:15:31 +0800

    修改ui

--- app/src/main/java/com/yxld/yxchuangxin/ui/adapter/wuye/LiuChengAdapter.java
+++ app/src/main/java/com/yxld/yxchuangxin/ui/adapter/wuye/LiuChengAdapter.java
@@ -23,26 +23,26 @@ public class LiuChengAdapter extends BaseQuickAdapter<LiuCheng, BaseViewHolder> 
     protected void convert(BaseViewHolder viewHolder, LiuCheng liuCheng) {
 
 
-        if (viewHolder.getAdapterPosition()==this.getData().size()-1){
-            viewHolder.setTextColor(R.id.tv_status, mContext.getResources().getColor(R.color.blue));
-          //  viewHolder.setBackgroundRes(R.id.view_bg, mContext.getResources().get(R.drawable.circle_blue));
-            // viewHolder.setTextColor(R.id.tv_name, mContext.getResources().getColor(R.color.blue));
-            //  viewHolder.setTextColor(R.id.tv_name，mContext.getResources().getColor(R.color.blue));
-            viewHolder.setBackgroundRes(R.id.view_bg, R.drawable.circle_blue);
-        }
+//        if (viewHolder.getAdapterPosition()==this.getData().size()-1){
+//            viewHolder.setTextColor(R.id.tv_status, mContext.getResources().getColor(R.color.blue));
+//          //  viewHolder.setBackgroundRes(R.id.view_bg, mContext.getResources().get(R.drawable.circle_blue));
+//            // viewHolder.setTextColor(R.id.tv_name, mContext.getResources().getColor(R.color.blue));
+//            //  viewHolder.setTextColor(R.id.tv_name，mContext.getResources().getColor(R.color.blue));
+//            viewHolder.setBackgroundRes(R.id.view_bg, R.drawable.circle_blue);
+//        }
         viewHolder.setText(R.id.tv_status,"报修状态："+ liuCheng.getZhuangtaiName());
         switch (liuCheng.getZhuangtai()) {
             case 0:
-               viewHolder.setText(R.id.tv_name,"报修人："+liuCheng.getName());
-               viewHolder.setText(R.id.tv_time,"时间："+liuCheng.getTime());
+               viewHolder.setText(R.id.tv_name,liuCheng.getName());
+               viewHolder.setText(R.id.tv_time,liuCheng.getTime());
                 break;
             case 1:
-                viewHolder.setText(R.id.tv_name,"负责人："+liuCheng.getName());
-                viewHolder.setText(R.id.tv_time,"时间："+liuCheng.getTime());
+                viewHolder.setText(R.id.tv_name,liuCheng.getName());
+                viewHolder.setText(R.id.tv_time,liuCheng.getTime());
                 break;
             case 2:
-                viewHolder.setText(R.id.tv_name,"维修人："+liuCheng.getName());
-                viewHolder.setText(R.id.tv_time,"时间："+liuCheng.getTime());
+                viewHolder.setText(R.id.tv_name,liuCheng.getName());
+                viewHolder.setText(R.id.tv_time,liuCheng.getTime());
                 break;
             case 3:
                 viewHolder.setVisible(R.id.tv_name,false);
@@ -53,12 +53,12 @@ public class LiuChengAdapter extends BaseQuickAdapter<LiuCheng, BaseViewHolder> 
                 viewHolder.setVisible(R.id.tv_time,false);
                 break;
             case 5:
-                viewHolder.setText(R.id.tv_name,"查验人："+liuCheng.getName());
-                viewHolder.setText(R.id.tv_time,"时间："+liuCheng.getTime());
+                viewHolder.setText(R.id.tv_name,liuCheng.getName());
+                viewHolder.setText(R.id.tv_time,liuCheng.getTime());
                 break;
             case 6:
-                viewHolder.setText(R.id.tv_name,"回访人："+liuCheng.getName());
-                viewHolder.setText(R.id.tv_time,"时间："+liuCheng.getTime());
+                viewHolder.setText(R.id.tv_name,liuCheng.getName());
+                viewHolder.setText(R.id.tv_time,liuCheng.getTime());
                 break;
             default:
                 break;
