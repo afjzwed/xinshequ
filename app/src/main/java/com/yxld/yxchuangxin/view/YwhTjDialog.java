62faeee83e23cd501125336dcd2da9e8bfd6c9c1
StoneXL <282346341@qq.com>
2018-11-09 09:18:05 +0800

    hu

---
+++ app/src/main/java/com/yxld/yxchuangxin/view/YwhTjDialog.java
@@ -0,0 +1,86 @@
+package com.yxld.yxchuangxin.view;
+
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.yxld.yxchuangxin.R;
+
+
+public class YwhTjDialog extends BaseDialog {
+
+
+	private Button bt_dialog_cancel;
+	private Button bt_dialog_confirm;
+	private OnConfirmListener onConfirmListener;
+
+
+
+	public YwhTjDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+//	public static void showDialog(Context context, String title, String message, OnConfirmListener confirmListener){
+//		YwhTjDialog dialog = new YwhTjDialog(context);
+//		dialog.setCanceledOnTouchOutside(false);
+//		dialog.setTitle(title);
+//		dialog.setMessage(message);
+//		dialog.setConfirmListener(confirmListener);
+//		dialog.show();
+//	}
+//
+	@Override
+	public void initView() {
+		setContentView(R.layout.dialog_tuijian_ly);
+		bt_dialog_cancel = (Button) findViewById(R.id.bt_dialog_cancel);
+		bt_dialog_confirm = (Button) findViewById(R.id.bt_dialog_confirm);
+
+	}
+
+	@Override
+	public void initListener() {
+		bt_dialog_cancel.setOnClickListener(this);
+		bt_dialog_confirm.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void initData() {
+
+
+	}
+
+	@Override
+	public void processClick(View v) {
+		switch (v.getId()) {
+		//如果取消按钮按下时，侦听存在，那么调用侦听的onCancel
+		case R.id.bt_dialog_cancel:
+			if(onConfirmListener != null){
+				onConfirmListener.onCancel();
+			}
+			break;
+		case R.id.bt_dialog_confirm:
+			if(onConfirmListener != null){
+				onConfirmListener.onConfirm();
+			}
+			break;
+			
+		}
+		//对话框消失
+		dismiss();
+	}
+	
+	public void setConfirmListener(OnConfirmListener confirmListener) {
+		this.onConfirmListener = confirmListener;
+	}
+
+
+
+	public interface OnConfirmListener{
+		void onCancel();
+		void onConfirm();
+	}
+}
