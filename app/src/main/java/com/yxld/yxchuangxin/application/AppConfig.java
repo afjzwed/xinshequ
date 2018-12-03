2293cb943363a58390e36a7c0103dd058b6e4c1c
StoneXL <282346341@qq.com>
2018-12-03 13:47:26 +0800

    阿里推送8.0支持

--- app/src/main/java/com/yxld/yxchuangxin/application/AppConfig.java
+++ app/src/main/java/com/yxld/yxchuangxin/application/AppConfig.java
@@ -2,9 +2,13 @@ package com.yxld.yxchuangxin.application;
 
 import android.app.ActivityManager;
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Process;
 import android.support.multidex.MultiDex;
 
@@ -199,6 +203,7 @@ public class AppConfig extends Application {
 //        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
 
         // 初始化阿里云推送
+        createNotificationChannel();
         initCloudChannel(this);
         //初始化广告位
         AdHub.initialize(this, "1874");
@@ -326,5 +331,27 @@ public class AppConfig extends Application {
             }
         });
     }
-
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            // 通知渠道的id
+            String id = "1";
+            // 用户可以看到的通知渠道的名字.
+            CharSequence name = "notification channel";
+            // 用户可以看到的通知渠道的描述
+            String description = "notification description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            // 配置通知渠道的属性
+            mChannel.setDescription(description);
+            // 设置通知出现时的闪灯（如果 android 设备支持的话）
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            // 设置通知出现时的震动（如果 android 设备支持的话）
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //最后在notificationmanager中创建该通知渠道
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+    }
 }
